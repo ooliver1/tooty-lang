8647e61f42368247bfc2cced5d02cb6686e8457c
ooliver1 <oliverwilkes2006@icloud.com>
2021-11-16 19:53:13 +0000

    -f--f-f-f-f-flags ish

--- main.cpp
+++ main.cpp
@@ -1,5 +1,50 @@
-#include <iostream>
+/*
+SPDX -License-Identifier: MIT
+Tooty-lang - A compiled and iterpreted language written in C++
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+MIT License
+
+Copyright (c) 2021-present Oliver Wilkes
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <stdio.h>
+#include <cstring>
+
+using std::strstr;
+
+int main(int argc, char** argv) {
+    const char* dbl_dash = "--";
+    const char* sgl_dash = "-";
+    int i;
+    for (i = 0; i < argc; i++) {
+        if (strstr(argv[i], dbl_dash) == argv[i]) {
+            printf("double %s\n", argv[i]);
+        }
+        else if (strstr(argv[i], sgl_dash) == argv[i]) {
+            printf("single %s\n", argv[i]);
+        }
+        else {
+            printf("%s\n", argv[i]);
+        }
+    }
+    // printf("argv[%d] = %s\n", i, argv[i]);
+    return 0;
 }
